2ec8bf457301bb037b7a0a70e254ba4b1b9716bb
Brian Kirk <bkirk6@gmail.com>
2019-09-18 18:06:17 -0400

    Add all files for Robofriends

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Patrick Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'CDeets4837@aol.com'
+    },
+    {
+        id: 6,
+        name: 'Mr. Dennis Schulist',
+        username: 'Leopoldo',
+        email: 'Karly_Dach@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwinn',
+        email: 'Telly.Hoggs@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runold',
+        username: 'NRunold',
+        email: 'Sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Richart',
+        username: 'Delphine',
+        email: 'Chaim_MCD@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Tsukitei Hosei',
+        username: 'Rakugan',
+        email: 'Sonny_Chiba_Hollywood.Doctor@gaki.jp'
+    }
+]
